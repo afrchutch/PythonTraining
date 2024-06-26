44e47f077c79c68470ed2fca175b99d571ab1a08
afrchutch <afrchutch@gmail.com>
2024-06-26 14:39:50 -0600

    Added function.py

--- function.py
+++ function.py
@@ -1,7 +1,7 @@
 # function = a block of code which is executed only when it is called.
 def hello(first_name, last_name, age):
     print("Hello "+first_name+" "+last_name)
-    print("You are "+str(age) + " years old")
+    print("You are "+str(age) + " years old.")
     print("Have a nice day!")
 
 
