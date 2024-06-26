f4713b1651bce025813f47b6940b07ea0e1d40e5
afrchutch <afrchutch@gmail.com>
2024-06-26 16:11:15 -0600

    Added kwargs.py

--- kwargs.py
+++ kwargs.py
@@ -2,10 +2,10 @@
 #            useful so that a function can accept a varying amount of keyword arguments
 
 def hello(**kwargs):  # "**kwargs" in this line can equal anything as long as the "**" precedes it
-#    print("Hello, " + kwargs['first'] + " " + kwargs['last'])
-     print("Hello,", end=" ")
-     for key, value in kwargs.items():
-         print(value, end=" ")
+    #    print("Hello, " + kwargs['first'] + " " + kwargs['last'])
+    print("Hello,", end=" ")
+    for key, value in kwargs.items():
+        print(value, end=" ")
 
 
 hello(title="Mr.", first="Cameron", middle="D", last="Hutchings")
